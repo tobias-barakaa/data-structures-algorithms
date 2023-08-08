fe54bfbf33f8cab7c21bbff2f90d8240bac58940
tobias-barakaa <tobiasbarakan@gmail.com>
2023-08-08 13:22:28 +0300

    array

---
+++ README.md
@@ -0,0 +1,6 @@
+In this repository we are going to learn more about DATA-STRUCTURES and Algorithms
+
+
+Array
+
+A collection of similar data elements
---
+++ file-array.c
@@ -0,0 +1,15 @@
+
+int main()
+{
+     int A[5];
+     int B[5] = {3,4,4,8,4,2};
+     // accessing an array;
+     // you come up with a variable to help you loop through
+
+     int i;
+     for(i = 0; i < 5; i++)
+     {
+          // print every element in B[5]
+          printf("%d", B[i]);
+     }
+}
